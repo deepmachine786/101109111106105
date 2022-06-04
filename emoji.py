b374f33e0bd10ab4b9b8f5870b38281e0195d179
deepmachine786 <mdshahid.12105824@lpu.in>
2022-06-04 15:33:15 +0530

    add all emoji in Apple Folder

--- emoji.py
+++ emoji.py
@@ -1,5 +1,4 @@
 from functools import wraps
-
 import requests 
 import os
 import bs4
@@ -25,18 +24,18 @@ def download_images(function):
     @wraps(function)
     def wrapper_function(*args):
         new_lists = function(*args) # calling the function ..
-        for images in range(len(new_lists)):
+        for images in range(len(new_lists)): 
             # check if the image sis endswith .png then downnload continue ..
             extension = new_lists[images]
             if extension is None:
-                continue
+                continue   
             else : extension = '.png'
             filename = str(images)+extension # get the file name t save the file ...
             get_images_dow = requests.get(new_lists[images], stream=True)
             with open(os.getcwd()+"\\Apple\\"+filename, "wb") as files:
                 shutil.copyfileobj(get_images_dow.raw,files)
         else: speak(" All file are Suceffuly Downloaded ")   
-    return wrapper_function
+    return wrapper_function 
 @download_images
 def find_url_images(images_lists: set): # this function is which imagrs has contains data-src with string link ...
     new_lists = [] 
